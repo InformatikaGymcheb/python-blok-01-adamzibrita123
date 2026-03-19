56baa42531500b3c3acfd78d686e8bd397963489
adamzibrita123 <adzibri@gymcheb.cz>
2026-03-19 13:25:23 +0100

    Add multiple tasks for user input and calculations

--- Lekce_03_Datové typy/main.py
+++ Lekce_03_Datové typy/main.py
@@ -4,6 +4,7 @@ x = int(input("Zadej první číslo: "))
 y = int(input("Zadej druhé číslo: "))
 
 print(f"Výsledek je: {x+y}")
+print()
 
 # ÚLOHA 2: Opakovač jména (Cíl: "Honza" a 3 -> "HonzaHonzaHonza")
 # ---------------------------------------------------------
@@ -11,18 +12,21 @@ jmeno = input("Zadej jméno: ")
 pocet = int(input("Zadej počet opakování: "))
 
 print(jmeno * pocet)
+print()
 
 # ÚLOHA 3: Rok narození (Cíl: 2026 - věk)
 # ---------------------------------------------------------
 vek = int(input("Zadej svůj věk: "))
 rok = 2026
 print(f"Váš přibližný rok narození je {rok - vek}")
+print()
 
 # ÚLOHA 4: Výplata (Cíl: mzda * hodiny)
 # ---------------------------------------------------------
 mzda = int(input("Zadej svou hodinovou mzdu: "))
 pocetHodin = int(input("Zadej počet odpracovaných hodin: "))
 print(f"Vaše celková odměna je {mzda * pocetHodin} Kč")
+print()
 
 # ÚLOHA 5: Magická matematika (Cíl: (x + 10) * 2)
 # ---------------------------------------------------------
